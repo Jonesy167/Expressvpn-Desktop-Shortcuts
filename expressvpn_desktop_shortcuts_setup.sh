6d7257b63a1bc481ff9f522705574c77343e2da2
Jonesy167 <30509908+Jonesy167@users.noreply.github.com>
2018-03-02 07:39:12 -0500

    Update expressvpn_desktop_shortcuts_setup.sh

--- expressvpn_desktop_shortcuts_setup.sh
+++ expressvpn_desktop_shortcuts_setup.sh
@@ -1,9 +1,9 @@
 user="$(whoami)"
 unzip expressvpn_desktop_shortcuts.zip
 sed -i "s/username/${user}/g" *.desktop #ensure .desktop files are pointing at bash script by replacing string 'username' with $user variable
-mkdir /home/$user/scripts2
-mv *.sh /home/$user/scripts2
-sudo chmod 755 /home/$user/scripts2/* #make scripts executable
+mkdir /home/$user/scripts
+mv *.sh /home/$user/scripts
+sudo chmod 755 /home/$user/scripts/* #make scripts executable
 sudo chown root:root *.png
 sudo mv *.png /var/lib/expressvpn
 mv * /home/$user/Desktop
