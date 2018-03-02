5f424f8d6075bf50fae738deb9ca5dc136b8782d
Jonesy167 <30509908+Jonesy167@users.noreply.github.com>
2018-03-02 08:15:34 -0500

    Update expressvpn_desktop_shortcuts_setup.sh

--- expressvpn_desktop_shortcuts_setup.sh
+++ expressvpn_desktop_shortcuts_setup.sh
@@ -1,12 +1,12 @@
 user="$(whoami)"
-unzip expressvpn_desktop_shortcuts.zip
+#unzip expressvpn_desktop_shortcuts.zip
 sed -i "s/username/${user}/g" *.desktop #ensure .desktop files are pointing at bash script by replacing string 'username' with $user variable
 mkdir /home/$user/Scripts
 mv *.sh /home/$user/Scripts
 sudo chmod 755 /home/$user/Scripts/* #make scripts executable
 sudo chown root:root *.png
 sudo mv *.png /var/lib/expressvpn
-mv * /home/$user/Desktop
+mv express* /home/$user/Desktop
 echo ""
 echo ""
 echo "setup complete, double click on expressvpn startup to start vpn, expressvpn disconnect to disconnect and expressvpn status to quickly see status of VPN"
